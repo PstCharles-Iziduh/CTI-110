3c5edab5670cd7362d61aa1436e748aaa833bb71
PstCharles-Iziduh <iziduhc5791@student.faytechcc.edu>
2026-02-09 07:13:07 -0500

    Add files via upload

---
+++ P1LAB1_IziduhCharles.py
@@ -0,0 +1,7 @@
+# Charles Iziduh
+#2/8/2026
+#P1LAB1_IziduhCharles.py
+#To get input from the user and display output
+name1 = input("Enter your First Name: ")
+name2 = input("Enter your Last Name: ")
+print("Hello," +  name1 + name2 +"!" + " Welcome to CTI-110")
